aff4ba252bd471b7547557417c775324f29922a6
Graham Pilkington <100288844+GrahamPilk@users.noreply.github.com>
2025-05-29 16:59:39 -0400

    App Layout Structure, NestedTabs, AuthenticationFLow

---
+++ ReactCourse/ReactNativeCourse/LayoutApp/bottomTab.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const FeedScreen = () => (
+  <View style={styles.layout}>
+    <Text style={styles.title}>Feed</Text>
+  </View>
+);
+
+const Tab = createBottomTabNavigator();
+
+export const AppNavigator = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Feed" component={FeedScreen} />
+    <Tab.Screen name="Catalog" component={FeedScreen} />
+  </Tab.Navigator>
+);
+
+const App = () => (
+  <NavigationContainer>
+    <AppNavigator />
+  </NavigationContainer>
+);
+
+export default App;
+
+const styles = StyleSheet.create({
+  layout: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 32,
+    marginBottom: 16,
+  },
+});
